73ba73162037b2969ad69ba62fd91bf9986e3176
JhonG <jhon.gonzalez@tecnologicoliceocristiano.edu.ec>
2024-07-16 21:16:58 -0500

    new file:   Suma.py

---
+++ Suma.py
@@ -0,0 +1,9 @@
+N1=int(input("Ingrese primer numero:"))
+N2=int(input("Ingrese segundo numero:"))
+
+S=N1+N2
+print(S)
+
+N3=int(input("Ingrese el multiplicador:"))
+Multiplicacion=S*N3
+print(Multiplicacion)
